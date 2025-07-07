4abb7d1b2c294e300073253d5a963a84602830a7
kindbyte <saglara.garyaeva0704@gmail.com>
2025-07-07 14:50:25 -0400

    edit

--- suspicious_activity.sql
+++ suspicious_activity.sql
@@ -1,6 +1,6 @@
-select userid, count(*) as reviews_in_one_day,
+select userid, rating, count(*) as reviews_in_one_day,
 to_timestamp(timestamp) :: date as review_date
 from amazon_ratings
-group by userid, review_date
-having count(*) >= 10
+group by userid, rating, review_date
+having count(*) >= 10 and rating in ('5', '1')
 order by reviews_in_one_day desc;
--- top_100_by_rating.sql
+++ top_100_by_rating.sql
@@ -1,20 +1,28 @@
 WITH suspicious_users AS (
- SELECT userid
- FROM amazon_ratings
- GROUP BY userid, TO_TIMESTAMP(timestamp)::DATE
- HAVING COUNT(*) >= 10
+  SELECT 
+    userid, 
+    rating, 
+    COUNT(*) AS reviews_in_one_day,
+    TO_TIMESTAMP(timestamp)::date AS review_date
+  FROM amazon_ratings
+  GROUP BY userid, rating, review_date
+  HAVING COUNT(*) >= 10 AND rating IN ('5', '1')
 ),
+
 clean_reviews AS (
- SELECT *
- FROM amazon_ratings
- WHERE userid NOT IN (SELECT userid FROM suspicious_users)
+  SELECT * 
+  FROM amazon_ratings
+  WHERE userid NOT IN (
+    SELECT userid FROM suspicious_users
+  )
 )
+
 SELECT 
- productId,
- COUNT(*) AS total_reviews,
- ROUND(AVG(rating), 2) AS avg_rating
+  productid, 
+  COUNT(*) AS total_reviews, 
+  ROUND(AVG(rating), 2) AS avg_rating
 FROM clean_reviews
-GROUP BY productId
-HAVING COUNT(*) >= 30
+GROUP BY productid
+HAVING COUNT(*) > 50
 ORDER BY avg_rating DESC
 LIMIT 100;
